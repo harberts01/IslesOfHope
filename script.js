9a6875850f93f45373b6012088c2837dd484b96a
harberts01 <109708014+harberts01@users.noreply.github.com>
2022-08-29 10:52:54 -0500

    Added JS Slider and logo bullet points

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+var currentIndex = 1;
+
+function setSlides(num) {
+    displaySlides (currentIndex += num)
+}
+
+let displaySlides = num => {
+    let x;
+    let slides = document.getElementsByClassName('imageSlides');
+    if(num > slides.length) {
+        currentIndex = 1
+    }
+    if (num < 1) {
+        currentIndex = slides.length
+    }
+    for(i=0; i < slides.length; i++){
+        slides[i].style.display= 'none';
+    }
+    slides[currentIndex - 1].style.display = "block";
+}
+
+displaySlides(currentIndex);
