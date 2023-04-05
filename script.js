b8d84388763233b181f450ed8f333d6286481a8f
Philip Harberts <phil@Philips-MacBook-Pro.local>
2023-04-05 14:05:39 -0500

    Adding Verse of the day scroller

--- script.js
+++ script.js
@@ -6,25 +6,65 @@ menu.addEventListener('click', function(){
     menuLinks.classList.toggle('active');
 })
 
-var currentIndex = 1;
+const API_KEY = `37a25eebfb17e7d9a32b530062c91228`; // Fill in with your own key.
+const verse = document.querySelector(`#verse-content`);
+const verseRef = document.querySelector(`#verse`);
 
-function setSlides(num) {
-    displaySlides (currentIndex += num)
-}
+const BIBLE_ID = `61fd76eafa1577c2-02`;
+const VERSES = [
+  `JER.29.11`,
+  `PSA.23`,
+  `1COR.4.4-8`,
+  `PHP.4.13`,
+  `JHN.3.16`,
+  `ROM.8.28`,
+  `ISA.41.10`,
+  `PSA.46.1`,
+  `GAL.5.22-23`,
+  `HEB.11.1`,
+  `2TI.1.7`,
+  `1COR.10.13`,
+  `PRO.22.6`,
+  `ISA.40.31`,
+  `JOS.1.9`,
+  `HEB.12.2`,
+  `MAT.11.28`,
+  `ROM.10.9-10`,
+  `PHP.2.3-4`,
+  `MAT.5.43-44`,
+];
 
-let displaySlides = num => {
-    let x;
-    let slides = document.getElementsByClassName('imageSlides');
-    if(num > slides.length) {
-        currentIndex = 1
-    }
-    if (num < 1) {
-        currentIndex = slides.length
-    }
-    for(i=0; i < slides.length; i++){
-        slides[i].style.display= 'none';
-    }
-    slides[currentIndex - 1].style.display = "block";
-}
+const verseIndex = Math.floor(Math.random() * VERSES.length);
+const verseID = VERSES[verseIndex];
 
-displaySlides(currentIndex);
+getResults(verseID).then((data) => {
+    const passage = data.passages[0];
+    verseRef.innerHTML = `<span><i>${passage.reference}</i></span>`;
+    verse.innerHTML = `<div class="text eb-container">${passage.content}</div>`;
+  });
+
+  function getResults(verseID) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.withCredentials = false;
+  
+      xhr.addEventListener(`readystatechange`, function () {
+        if (this.readyState === this.DONE) {
+          const { data, meta } = JSON.parse(this.responseText);
+  
+          _BAPI.t(meta.fumsId);
+          resolve(data);
+        }
+      });
+  
+      xhr.open(
+        `GET`,
+        `https://api.scripture.api.bible/v1/bibles/${BIBLE_ID}/search?query=${verseID}`
+      );
+      xhr.setRequestHeader(`api-key`, API_KEY);
+  
+      xhr.onerror = () => reject(xhr.statusText);
+  
+      xhr.send();
+    });
+  }
